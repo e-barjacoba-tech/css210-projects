52d47823eee5d7f66b26c6a2703fc72ed1220303
e-barjacoba-tech <e.barjacoba.tech@gmail.com>
2026-01-15 00:28:46 -0300

    Second exercise (Week1) completed!

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        string letter = "A";
+
+        if (gradeNumber >= 90)
+        {
+            Console.Write($"You got an {letter}! ");
+        }
+        else if (gradeNumber >= 80 && gradeNumber < 90)
+        {
+           letter = "B"; 
+           Console.Write($"You got a {letter}! "); 
+        }
+        else if (gradeNumber >= 70 && gradeNumber < 80)
+        {
+           letter = "C";  
+           Console.Write($"You got a {letter}... "); 
+        }
+        else if (gradeNumber >= 60 && gradeNumber < 70)
+        {
+           letter = "D"; 
+           Console.Write($"You got a {letter}... "); 
+        }
+        else if (gradeNumber < 60)
+        {
+           letter = "F"; 
+           Console.Write($"... you got an {letter}... "); 
+        }
+        
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("You passed the class :)");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Try again next semester");  
+        }
     }
 }
